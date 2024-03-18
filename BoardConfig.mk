3db792ac29060d58ceb3b1e4cd2bdbdf5d256128
Sarawut28T <163823851+Sarawut28T@users.noreply.github.com>
2024-03-18 17:02:13 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X676C
+DEVICE_PATH := device/infinix/Infinix-X6837
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -44,7 +44,7 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := Infinix-X676C
+TARGET_BOOTLOADER_BOARD_NAME := Infinix-X6837
 TARGET_NO_BOOTLOADER := true
 
 # Display
@@ -62,8 +62,8 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-TARGET_KERNEL_CONFIG := Infinix-X676C_defconfig
-TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-X676C
+TARGET_KERNEL_CONFIG := Infinix-X6837_defconfig
+TARGET_KERNEL_SOURCE := kernel/infinix/Infinix-X6837
 
 # Kernel - prebuilt
 TARGET_FORCE_PREBUILT_KERNEL := true
