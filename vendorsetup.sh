0f90463158184e8dbe1f3e1bbc9d12447abd81ea
Sarawut28T <163823851+Sarawut28T@users.noreply.github.com>
2024-03-18 17:09:04 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Infinix-X676C-user
-add_lunch_combo omni_Infinix-X676C-userdebug
-add_lunch_combo omni_Infinix-X676C-eng
+add_lunch_combo omni_Infinix-X6837-user
+add_lunch_combo omni_Infinix-X6837-userdebug
+add_lunch_combo omni_Infinix-X6837-eng
